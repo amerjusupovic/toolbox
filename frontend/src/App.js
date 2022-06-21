00a8473f6f4cad229dbea2a152d41698f46b3380
Amer Jusupovic <amerjusupovic00@gmail.com>
2022-06-21 17:57:08 -0500

    express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,27 @@ import './App.scss';
 import { Input } from 'antd';
 import { useState } from 'react';
 const axios = require("axios");
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/shorten', (req, res) => {
+  console.log(req)
+  res = await axios.get('https://cutt.ly/api/api.php', {params: {key: process.env.CUTTLY_API_KEY, short: "bestdex.vercel.app", name: "cutt.ly/amerjusupovic"}, 
+  headers: {"Access-Control-Allow-Origin": "*"}}).then(function (response) {
+    console.log(response.data)
+    return response.data.shortLink;
+  }).catch(function (error) {
+    console.error(error);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 function App() {
   const [shortURL, setShortURL] = useState("");
@@ -10,10 +31,9 @@ function App() {
   async function shortenURL(shorten, alias){  
     let data = "";
   
-    data = await axios.get('https://cutt.ly/api/api.php', {params: {key: process.env.CUTTLY_API_KEY, short: shorten, name: alias}, 
-      headers: {"Access-Control-Allow-Origin": "*"}}).then(function (response) {
-        console.log(response.data)
-        return response.data.shortLink;
+    data = await axios.get('/shorten', {params: {key: process.env.CUTTLY_API_KEY, short: shorten, name: alias}}).then(function (response) {
+      console.log(response.data)
+      return response.data.shortLink;
     }).catch(function (error) {
       console.error(error);
     });
