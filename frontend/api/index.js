bbdacd2dae6523c35c0286621e6a60e78d3cb4e6
Amer Jusupovic <amerjusupovic00@gmail.com>
2022-06-27 13:56:32 -0500

    try fix express

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
@@ -17,8 +17,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(
   path.join(__dirname,"../build")));
